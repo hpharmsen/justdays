31086195b6cded193eb3d56fc626aab54406f5e8
hpharmsen <hp@harmsen.nl>
2024-06-21 12:26:10 +0200

    publish Fri Jun 21 12:26:10 CEST 2024

--- publish.sh
+++ publish.sh
@@ -1,7 +1,17 @@
-rm dist/*
-python bumpversion.py patch
+start_time=$(date +%s)
+pip install --upgrade pip
+pip install twine build
+/bin/rm -f dist/*
+export VERSION=`python bumpversion.py -v patch`
 python -m build
 twine upload dist/*
 git commit -v -a -m "publish `date`"
-git push
-echo "run pip install --no-cache --force-reinstall justdays"
+git tag -a $VERSION -m "version $VERSION"
+git push origin $VERSION
+duration=$(($(date +%s) - start_time))
+echo "${GREEN}Published in $duration secs${NC}"
+echo ""
+echo "run:"
+echo "pip install git+https://github.com/hpharmsen/justdays@$VERSION"
+echo "of:"
+echo "python -m pip install --upgrade pip; pip install justdays==$VERSION"
